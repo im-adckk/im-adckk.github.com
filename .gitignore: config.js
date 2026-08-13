64e6a7a7e01a120e431a1845808b7cf63994ab83
im-adckk <adcdriving@gmail.com>
2026-08-13 12:06:45 +0800

    Add config.js with configuration settings

---
+++ .gitignore: config.js
@@ -0,0 +1,5 @@
+const CONFIG = {
+    ADMIN_PASSWORD: 'admin123',
+    SUPABASE_URL: 'https://yrrinzreyafiowehhhon.supabase.co',
+    SUPABASE_ANON_KEY: 'sb_publishable_4MnAXo4yxHMQX7fSn7hQjA_qV2X7t7o'
+};
